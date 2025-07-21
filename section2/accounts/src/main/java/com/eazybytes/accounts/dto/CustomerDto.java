9b41553cf055bdb86e787dedfc73f5abf8bd37c8
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-07-21 19:41:06 +0100

    documenation the dto entity

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -8,19 +9,40 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account details"
+)
 public class CustomerDto {
 
+
+    @Schema(
+            description = "Name of the customer",
+            example = "Hicham Azeroual"
+
+    )
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 5,max = 30, message = "Name should have at least 2 characters")
     private String name;
 
+    @Schema(
+            description = "Email of the customer",
+            example = "hicham.azeroual@in.com"
+    )
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
     private String email;
 
+    @Schema(
+            description = "Mobile Number of the customer",
+            example = "1234567890"
+    )
     @NotEmpty(message = "Mobile Number should not be empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number should have 10 digits")
     private String mobileNumber;
 
+    @Schema(
+            description = "Account Details of the customer"
+    )
     private AccountsDto accountsDto;
 }
--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -8,11 +9,26 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
 public class ErrorResponseDto {
 
+     @Schema(
+             description = "API path where the error occurred"
+     )
      private String apiPath;
+     @Schema(
+             description = "HTTP status code of the error"
+     )
      private HttpStatus errorCode;
+     @Schema(
+             description = "Error message of the error"
+     )
      private String errorMessage;
+     @Schema(
+             description = "Timestamp of the error")
      private LocalDateTime errorTime;
 
 
