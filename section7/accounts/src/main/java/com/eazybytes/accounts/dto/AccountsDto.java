867558089cb3f8bfc03c13ab83f20aec80309dfa
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-08-31 14:55:08 +0100

    add mysql databse for each  microservice and delete the use of the h2 using docker

---
+++ section7/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,36 @@
+package com.eazybytes.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Accounts",
+        description = "Account to hold Account information"
+)
+public class AccountsDto {
+
+    @Schema(
+            description = "Account Number of the customer",
+            example = "1234567890"
+    )
+    @NotEmpty(message = "Account Number should not be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account Number should have 10 digits")
+    private Long accountNumber;
+
+    @Schema(
+            description = "Account Type of the customer",
+            example = "Savings"
+    )
+    @NotEmpty(message = "Account Type should not be empty")
+    private String accountType;
+
+    @Schema(
+            description = "Branch Address of the customer",
+            example = "123 Main Street, New York"
+    )
+    @NotEmpty(message = "Branch Address should not be empty")
+    private String branchAddress;
+}
